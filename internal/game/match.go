a263d42ae5c4438f20994648bda5635c3949f62d
Sam Martin <swordeh@users.noreply.github.com>
2024-08-27 17:31:39 +0100

    Add match control functions.

--- internal/game/match.go
+++ internal/game/match.go
@@ -1,9 +1,11 @@
 package game
 
+import "log"
+
 // Match represents a game between two connected players
 type Match struct {
 	Players []*Player
-	Status  string //WAITING, STARTED, FINISHED or CANCELLED
+	Status  string //WAITING, STARTED
 }
 
 func NewMatch() *Match {
@@ -15,12 +17,18 @@ func (m *Match) AddPlayer(player *Player) {
 	m.Players = append(m.Players, player)
 }
 
+// RemovePlayer will remove a player from the list of Players for a match.
+// If the match is not in the WAITING state (it has started) then the match will be cancelled.
 func (m *Match) RemovePlayer(player *Player) {
 	for i, p := range m.Players {
 		if p == player {
 			m.Players = append(m.Players[:i], m.Players[i+1:]...)
 			break
 		}
+	}
+	if m.Status != "WAITING" {
+		log.Println("match not in waiting state, cancel")
+		m.Cancel()
 	}
 }
 
@@ -34,8 +42,21 @@ func (m *Match) Start() {
 
 func (m *Match) Cancel() {
 	m.Status = "CANCELLED"
+	for _, player := range m.Players {
+		player.Connection.Send("Match cancelled\n")
+		player.SetInGameStatus(false)
+	}
 }
 
 func (m *Match) End() {
 	m.Status = "FINISHED"
+}
+
+func (m *Match) Opponent(p *Player) *Player {
+	for i, player := range m.Players {
+		if player != p {
+			return m.Players[i]
+		}
+	}
+	return nil
 }
